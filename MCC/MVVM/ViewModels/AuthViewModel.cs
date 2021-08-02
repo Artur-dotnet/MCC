f484ad45dff4dae3a012306a7a0b92116f04cbdd
Artur358 <itgamevenpc@gmail.com>
2021-08-03 00:41:16 +0300

    first commit

---
+++ MCC/MVVM/ViewModels/AuthViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MCC.Commands;
+
+namespace MCC.MVVM.ViewModels
+{
+    class AuthViewModel : BaseViewModel
+    {
+
+        private SignIn signInVM;
+        private SignUp signUpVM;
+
+
+
+        private object _currentContent;
+        public object CurrentContent
+        {
+            get => _currentContent;
+            set
+            {
+                _currentContent = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public AuthViewModel()
+        {
+            signInVM = new SignIn();
+            signUpVM = new SignUp();
+            CurrentContent = signInVM;
+        }
+
+
+        private RelayCommand _signInCommand;
+        public RelayCommand SignInCommand
+        {
+            get => _signInCommand ?? (_signInCommand = new RelayCommand(obj =>
+            {
+                CurrentContent = signInVM;
+            }));
+        }
+
+
+        private RelayCommand _signUpCommand;
+        public RelayCommand SignUpCommand
+        {
+            get => _signUpCommand ?? (_signUpCommand = new RelayCommand(obj =>
+            {
+                CurrentContent = signUpVM;
+            }));
+        }
+
+    }
+}
