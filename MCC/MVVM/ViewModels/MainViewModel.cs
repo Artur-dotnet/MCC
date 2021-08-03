f0ba1259cb00e42f32bf6d95b433d4c445b860c1
student <student@K09-04>
2021-08-03 18:48:56 +0300

    test commit

--- MCC/MVVM/ViewModels/MainViewModel.cs
+++ MCC/MVVM/ViewModels/MainViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MCC.MVVM.ViewModels
 {
-    class SignUp:BaseViewModel
+    class MainViewModel:BaseViewModel
     {
     }
 }
--- MCC/MVVM/ViewModels/SignInViewModel.cs
+++ MCC/MVVM/ViewModels/SignInViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MCC.MVVM.ViewModels
 {
-    class SignIn:BaseViewModel
+    class SignInViewModel:BaseViewModel
     {
     }
 }
